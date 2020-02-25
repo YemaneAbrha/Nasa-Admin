fb06033a326f060029896f18bb0765d1464ab816
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-25 12:02:00 +0300

    Completing Form UI

--- lib/Presentation/body/message.dart
+++ lib/Presentation/body/message.dart
@@ -9,11 +9,54 @@ class MessagePage extends StatefulWidget {
 }
 
 class _MessagePageState extends State<MessagePage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Message"),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: Icon(Icons.message),
+              labelText: "Message *",
+            ),
+            maxLength: 80,
+            validator: (value) {
+              if (value.isEmpty) {
+                return "Message must not empty";
+              }
+              if (value.length > 80 || value.length < 10) {
+                return "Enter Character b.n 10 and 80";
+              } else
+                return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: RaisedButton(
+                child: Text(
+                  "Send",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                color: Color.fromRGBO(0, 141, 81, 1.0),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context)
+                        .showSnackBar(SnackBar(content: Text('Processing...')));
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
