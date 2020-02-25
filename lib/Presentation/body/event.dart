491d89143789241a7b2c15698c44ac2a4d8ddb68
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-25 10:00:59 +0300

    Adding File

---
+++ lib/Presentation/body/event.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//From nasaAcadamy package.
+import 'package:nasaadmin/Presentation/common/basicappbar.dart';
+
+class EventPage extends StatefulWidget {
+  @override
+  _EventPageState createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(235, 237, 240, 1.0),
+      appBar: BasicAppBar.getAppBar(context),
+      body: Container(),
+      // drawer: SideBarDrawer(),
+      // bottomNavigationBar: ButtomNavigationBarContainer(),
+    );
+  }
+}
