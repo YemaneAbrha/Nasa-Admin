8078afbdf080d6907240cb10ecbe7fa2174623b0
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-25 12:28:23 +0300

    Adding http

---
+++ lib/Model/Json/data.dart
@@ -0,0 +1,13 @@
+class Event {
+  final String id;
+  final String title;
+  final String body;
+  final String image;
+  Event(this.id, this.title, this.body, this.image);
+}
+
+class Message {
+  final int id;
+  final String body;
+  Message(this.id, this.body);
+}
