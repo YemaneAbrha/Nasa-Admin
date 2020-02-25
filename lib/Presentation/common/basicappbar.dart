2850ac0565cf6b46bec68991e2475471c60e2199
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-25 10:04:43 +0300

    Adding File

---
+++ lib/Presentation/common/basicappbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BasicAppBar {
+  static getAppBar(BuildContext context) {
+    return AppBar(
+      title: Text("ናሳ ት/ቤት"),
+      backgroundColor: Color.fromRGBO(0, 141, 81, 1.0),
+    );
+  }
+}
