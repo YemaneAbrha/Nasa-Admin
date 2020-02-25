dc141b4e317ad724c7741afff9182c3446763754
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-25 10:27:26 +0300

    Removing App Bar

--- lib/Presentation/body/message.dart
+++ lib/Presentation/body/message.dart
@@ -13,7 +13,7 @@ class _MessagePageState extends State<MessagePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(235, 237, 240, 1.0),
-      appBar: BasicAppBar.getAppBar(context),
+
       body: Container(),
       // drawer: SideBarDrawer(),
       // bottomNavigationBar: ButtomNavigationBarContainer(),
